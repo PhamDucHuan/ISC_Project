b45f8190411ef1ef994b0b1882c0c243dec9b9a4
Phạm Đức Huân <155977706+PhamDucHuan@users.noreply.github.com>
2025-07-25 21:36:39 +0700

    Update Program

--- ISC_Project/Program.cs
+++ ISC_Project/Program.cs
@@ -34,73 +34,76 @@ namespace ISC_Project
             builder.Services.AddScoped<IJwtService, JwtService>();
 
             // Registering various services
-            builder.Services.AddScoped<IChatAIService, ChatAIService>();
-            builder.Services.AddScoped<IUserManagementService, UserManagementService>();
-            builder.Services.AddScoped<IRewardDisciplineService, RewardDisciplineService>();
-            builder.Services.AddScoped<IQuestionBankService, QuestionBankService>();
-            builder.Services.AddScoped<IExemptionService, ExemptionService>();
-
-            builder.Services.AddScoped<IRoleService, RoleService>();
-            builder.Services.AddScoped<IStudentProfileService, StudentProfileService>();
-            builder.Services.AddScoped<ISchoolProfileService, SchoolProfileService>();
-            builder.Services.AddScoped<ITeamDepartmentService, TeamDepartmentService>();
-            builder.Services.AddScoped<IClassTypeService, ClassTypeService>();
-            builder.Services.AddScoped<IClassService, ClassService>();
-            builder.Services.AddScoped<IClassSubjectService, ClassSubjectService>();
-            builder.Services.AddScoped<IClassHistoryService, ClassHistoryService>();
-            builder.Services.AddScoped<ISubjectService, SubjectService>();
-
             builder.Services.AddScoped<IExamService, ExamService>();
-
-            builder.Services.AddScoped<ITeacherProfileService, TeacherProfileService>();
-            builder.Services.AddScoped<ISchoolYearService, SchoolYearService>();
-            builder.Services.AddScoped<ICourseCategoryService, CourseCategoryService>();
-            builder.Services.AddScoped<ICourseService, CourseService>();
-            builder.Services.AddScoped<ICourseOfferingService, CourseOfferingService>();
-            builder.Services.AddScoped<ICourseLessonService, CourseLessonService>();
-            builder.Services.AddScoped<IRegistrationService, RegistrationService>();
-
-            builder.Services.AddScoped<IDiscussionThreadService, DiscussionThreadService>();
-            builder.Services.AddScoped<IThreadPostService, ThreadPostService>();
-            builder.Services.AddScoped<ILiveChatMessageService, LiveChatMessageService>();
-            builder.Services.AddScoped<ILiveSessionService, LiveSessionService>();
-            builder.Services.AddScoped<ICampusService, CampusService>();
-            builder.Services.AddScoped<IEnrollmentService, EnrollmentService>();
-
-            builder.Services.AddScoped<ISyllabusTopicService, SyllabusTopicService>();
-            builder.Services.AddScoped<ISystemSettingService, SystemSettingService>();
-            builder.Services.AddScoped<ITotalCoursesTakenService, TotalCoursesTakenService>();
-            builder.Services.AddScoped<ITrainingLevelService, TrainingLevelService>();
-            builder.Services.AddScoped<IUpcomingClassService, UpcomingClassService>();
-            builder.Services.AddScoped<IWorkHistoryService, WorkHistoryService>();
 
             builder.Services.AddScoped<IAcceptingSchoolTransferService, AcceptingSchoolTransferService>();
             builder.Services.AddScoped<IAssessmentPartService, AssessmentPartService>();
             builder.Services.AddScoped<IAssessmentQuestionService, AssessmentQuestionService>();
-            builder.Services.AddScoped<IAssignmentService, AssignmentService>();
             builder.Services.AddScoped<IAssignmentGroupService, AssignmentGroupService>();
+            builder.Services.AddScoped<IAssignmentService, AssignmentService>();
+            builder.Services.AddScoped<ICampusService, CampusService>();
+            builder.Services.AddScoped<IChatAIService, ChatAIService>();
+
             builder.Services.AddScoped<IClassDetailService, ClassDetailService>();
+            builder.Services.AddScoped<IClassHistoryService, ClassHistoryService>();
             builder.Services.AddScoped<IClassroomSettingService, ClassroomSettingService>();
+            builder.Services.AddScoped<IClassService, ClassService>();
+            builder.Services.AddScoped<IClassSubjectService, ClassSubjectService>();
+            builder.Services.AddScoped<IClassTypeService, ClassTypeService>();
+
+            builder.Services.AddScoped<ICourseCategoryService, CourseCategoryService>();
+            builder.Services.AddScoped<ICourseLessonService, CourseLessonService>();
+            builder.Services.AddScoped<ICourseOfferingService, CourseOfferingService>();
+            builder.Services.AddScoped<ICourseService, CourseService>();
             builder.Services.AddScoped<ICoursesLearnedService, CoursesLearnedService>();
 
-            builder.Services.AddScoped<IRelativesInformationService, RelativesInformationService>();
-            builder.Services.AddScoped<IRelativesInformationService, RelativesInformationService>();
+            builder.Services.AddScoped<IDiscussionThreadService, DiscussionThreadService>();
+            builder.Services.AddScoped<IEmploymentHistoryService, EmploymentHistoryService>();
+            builder.Services.AddScoped<IEnrollmentService, EnrollmentService>();
+            builder.Services.AddScoped<IExemptionService, ExemptionService>();
+            builder.Services.AddScoped<IFacultyStudyBlockService, FacultyStudyBlockService>();
+
+            builder.Services.AddScoped<IFamilyInformationService, FamilyInformationService>();
+            builder.Services.AddScoped<IGradeService, GradeService>();
+            builder.Services.AddScoped<ILiveChatMessageService, LiveChatMessageService>();
+            builder.Services.AddScoped<ILiveSessionService, LiveSessionService>();
+            builder.Services.AddScoped<INotificationService, NotificationService>();
 
             builder.Services.AddScoped<IPastClassesService, PastClassesService>();
             builder.Services.AddScoped<IPermissionService, PermissionService>();
-            builder.Services.AddScoped<IPermissionService, PermissionService>();
-
+            builder.Services.AddScoped<IPrivateChatService, PrivateChatService>();
             builder.Services.AddScoped<IQualificationsService, QualificationService>();
+            builder.Services.AddScoped<IQuestionBankService, QuestionBankService>();
+
+            builder.Services.AddScoped<IRegistrationService, RegistrationService>();
+            builder.Services.AddScoped<IRelativesInformationService, RelativesInformationService>();
             builder.Services.AddScoped<IReservedService, ReservedService>();
-            builder.Services.AddScoped<INotificationService, NotificationService>();
+            builder.Services.AddScoped<IRewardDisciplineService, RewardDisciplineService>();
             builder.Services.AddScoped<IRolePermissionService, RolePermissionService>();
+
+            builder.Services.AddScoped<IRoleService, RoleService>();
+            builder.Services.AddScoped<ISchoolProfileService, SchoolProfileService>();
+            builder.Services.AddScoped<ISchoolYearService, SchoolYearService>();
             builder.Services.AddScoped<IScoreService, ScoreService>();
             builder.Services.AddScoped<ISemesterService, SemesterService>();
+            builder.Services.AddScoped<IStudentProfileService, StudentProfileService>();
+
             builder.Services.AddScoped<IStudentsChangeClassService, StudentsChangeClassService>();
             builder.Services.AddScoped<IStudentsChangeSchoolService, StudentsChangeSchoolService>();
             builder.Services.AddScoped<ISubjectService, SubjectService>();
-            builder.Services.AddScoped<IGradeService, GradeService>();
-            builder.Services.AddScoped<IFacultyStudyBlockService, FacultyStudyBlockService>();
+            builder.Services.AddScoped<ISubjectTypeService, SubjectTypeService>();
+            builder.Services.AddScoped<ISyllabusTopicService, SyllabusTopicService>();
+
+            builder.Services.AddScoped<ISystemSettingService, SystemSettingService>();
+            builder.Services.AddScoped<ITeacherProfileService, TeacherProfileService>();
+            builder.Services.AddScoped<ITeamDepartmentService, TeamDepartmentService>();
+            builder.Services.AddScoped<IThreadPostService, ThreadPostService>();
+            builder.Services.AddScoped<ITotalCoursesTakenService, TotalCoursesTakenService>();
+
+            builder.Services.AddScoped<ITrainingLevelService, TrainingLevelService>();
+            builder.Services.AddScoped<IUpcomingClassService, UpcomingClassService>();
+            builder.Services.AddScoped<IUserManagementService, UserManagementService>();
+            builder.Services.AddScoped<IWorkHistoryService, WorkHistoryService>();
 
 #endregion
 
